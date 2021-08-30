46ce0219fc1efbe423036f749223c64a9d754394
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2021-08-30 22:11:48 +0200

    Initial commit

---
+++ kademlia.go
@@ -0,0 +1,16 @@
+package D7024E
+
+type Kademlia struct {
+}
+
+func (kademlia *Kademlia) LookupContact(target *Contact) {
+	// TODO
+}
+
+func (kademlia *Kademlia) LookupData(hash string) {
+	// TODO
+}
+
+func (kademlia *Kademlia) Store(data []byte) {
+	// TODO
+}
---
+++ network.go
@@ -0,0 +1,24 @@
+package D7024E
+
+type Network struct {
+}
+
+func Listen(ip string, port int) {
+	// TODO
+}
+
+func (network *Network) SendPingMessage(contact *Contact) {
+	// TODO
+}
+
+func (network *Network) SendFindContactMessage(contact *Contact) {
+	// TODO
+}
+
+func (network *Network) SendFindDataMessage(hash string) {
+	// TODO
+}
+
+func (network *Network) SendStoreMessage(data []byte) {
+	// TODO
+}
