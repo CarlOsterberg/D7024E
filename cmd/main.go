6478abbb4b24221161266086841408a1c6a65005
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2021-09-09 14:42:44 +0200

    module

--- cmd/main.go
+++ cmd/main.go
@@ -4,40 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	//_ "github.com/CarlOsterberg/D7024E/kademlia"
+	//kademlia "./kademlia"
 	udp "github.com/CarlOsterberg/D7024E/udp"
 	"strings"
 )
 
 func main() {
 	comms := make(chan string)
-	go udp.Server(comms)
 	reader := bufio.NewReader(os.Stdin)
-	run := true
-	for run {
+	for {
 		fmt.Print("-> ")
 		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
 		switch strings.Replace(text, "\n", "", -1) {
-		case "send":
-			fmt.Print("IP: ")
-			ip, _ := reader.ReadString('\n')
-			ip = strings.Replace(ip, "\n", "", -1)
-			fmt.Print("msg: ")
-			msg, _ := reader.ReadString('\n')
-			msg = strings.Replace(msg, "\n", "", -1)
-			udp.Client(ip, msg)
-		case "self":
-			fmt.Printf("%v\n", udp.GetOutboundIP())
-		case "q":
-			run = false
-		case "ping":
-			fmt.Print("IP: ")
-			ip, _ := reader.ReadString('\n')
-			ip = strings.Replace(ip, "\n", "", -1)
-			self := udp.GetOutboundIP().String()
-			udp.Client(ip, string(self)+":1234")
+		case "123":
+			udp.Test()
+		case "server":
+			go func() {
+				udp.Server(comms)
+			}()
+		case "client":
+			udp.Client()
+		case "1":
+			fmt.Printf("amendurå")
 		default:
-			fmt.Printf("Not a command\n")
+			fmt.Printf("You wrote: %v", text)
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/CarlOsterberg/D7024E
 
 go 1.17
+
+require (
+	github.com/CarlOsterberg/D7024E/udp v0.1.0
+)
