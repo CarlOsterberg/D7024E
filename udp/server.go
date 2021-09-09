58d940a6de8e1d74c1cc96b1521e7fb209bc82c0
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2021-09-09 16:53:28 +0200

    working udp server + client

--- udp/server.go
+++ udp/server.go
@@ -1,42 +1,40 @@
 package udp
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 )
 
 //https://stackoverflow.com/questions/26028700/write-to-client-udp-socket-in-go
 
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) (result string) {
-	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_, err := conn.WriteToUDP([]byte("Ty 4 msg"), addr)
 	if err != nil {
 		fmt.Printf("Couldn't send response %v", err)
-		return "Fail"
-	} else {
-		return "Success"
 	}
 }
 
 func Server(comms chan string) {
-	p := make([]byte, 2048)
+	buffer := make([]byte, 2048)
+	//setting IP to nil makes it listen to all available ips
 	addr := net.UDPAddr{
 		Port: 1234,
-		IP:   net.ParseIP("127.0.0.1"),
+		IP:   nil,
 	}
-	ser, err := net.ListenUDP("udp", &addr)
+	server, err := net.ListenUDP("udp", &addr)
 	if err != nil {
 		fmt.Printf("Some error %v\n", err)
 		return
 	}
 	for {
-		_, remoteaddr, err := ser.ReadFromUDP(p)
-		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+		_, remoteaddr, err := server.ReadFromUDP(buffer)
+		n := bytes.IndexByte(buffer[:], 0)
+		fmt.Printf("Server recv: %v from: %s \n", string(buffer[:n]), remoteaddr)
 		if err != nil {
 			fmt.Printf("Some error  %v", err)
 			continue
 		}
-		go func() {
-			fmt.Printf("Did work? %v", sendResponse(ser, remoteaddr))
-		}()
+		go sendResponse(server, remoteaddr)
 	}
 }
