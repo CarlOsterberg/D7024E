30abe00ecd6a386771ca504f44e34b68ad70b1ea
isak8748 <isasun-7@student.ltu.se>
2021-09-10 16:28:44 +0200

    node lookup

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -1,11 +1,38 @@
 package src
 
+const alpha = 3
+const k = 20
+
 type Kademlia struct {
+	routingTable RoutingTable
+	network Network
 }
 
+func NewKademlia(me Contact) *Kademlia{
+	kademlia := &Kademlia{}
+	rt := NewRoutingTable(me)
+	kademlia.routingTable = *rt
+	network := &Network{}
+	kademlia.network = *network
+	return kademlia
+}
+
+// LookupContact performs the node-lookup recursively
 func (kademlia *Kademlia) LookupContact(target *Contact) {
 	// TODO
+	closestContacts := kademlia.routingTable.FindClosestContacts(target.ID, alpha)
+	for i := 0; i < len(closestContacts); i++ {
+		kademlia.network.SendFindContactMessage(&closestContacts[i])
+	}
 }
+
+// KClosestNodes Finds the k closest nodes to the target
+func (kademlia *Kademlia) KClosestNodes(target *Contact) []Contact{
+	closestContacts := kademlia.routingTable.FindClosestContacts(target.ID, k)
+	return closestContacts
+}
+
+
 
 func (kademlia *Kademlia) LookupData(hash string) {
 	// TODO
