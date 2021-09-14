a57625cf5d3b367c8577672a1889b39c774aedf5
isak8748 <isasun-7@student.ltu.se>
2021-09-14 15:33:00 +0200

    lookup code

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -1,34 +1,43 @@
 package kademlia
 
-import "crypto/sha1"
+const alpha = 3
+const k = 20
 
 type Kademlia struct {
-	valueMap  map[string][]byte
-}
-func NewKademlia() *Kademlia{
-	kademlia := &Kademlia{}
-	valueMap := make(map[string][]byte)
-	kademlia.valueMap = valueMap
-	return kademlia
+routingTable RoutingTable
+network Network
 }
 
+func NewKademlia(me Contact) *Kademlia{
+kademlia := &Kademlia{}
+rt := NewRoutingTable(me)
+kademlia.routingTable = *rt
+network := &Network{}
+kademlia.network = *network
+return kademlia
+}
+
+// LookupContact performs the node-lookup recursively
 func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
+// TODO
+closestContacts := kademlia.routingTable.FindClosestContacts(target.ID, alpha)
+for i := 0; i < len(closestContacts); i++ {
+kademlia.network.SendFindContactMessage(&closestContacts[i])
+}
 }
 
-func (kademlia *Kademlia) LookupData(hash string) []byte{
+// KClosestNodes Finds the k closest nodes to the target
+func (kademlia *Kademlia) KClosestNodes(target *Contact) []Contact{
+closestContacts := kademlia.routingTable.FindClosestContacts(target.ID, k)
+return closestContacts
+}
 
-	//fmt.Println("map:", string(kademlia.valueMap[hash]))
-	return kademlia.valueMap[hash]
+
+
+func (kademlia *Kademlia) LookupData(hash string) {
+// TODO
 }
 
 func (kademlia *Kademlia) Store(data []byte) {
-	// https://gobyexample.com/sha1-hashes
-	// https://gobyexample.com/maps
-	key :=  sha1.New()
-	key.Write(data)
-	var id = string(key.Sum(nil))
-	kademlia.valueMap[id] = data
-
-
+// TODO
 }
